474cf2551d12eef4bf1017aa6f31557b594efba4
WHF <6109118198@email.ncu.edu.cn>
2020-11-29 14:49:35 +0800

    修改

--- src/main/java/team/cms/repository/DriverRepository.java
+++ src/main/java/team/cms/repository/DriverRepository.java
@@ -1,7 +1,12 @@
 package team.cms.repository;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import team.cms.entity.Driver;
 
+@Mapper
+@Repository
 public interface DriverRepository {
     Driver getDriverByAccountId(int accountId);
 
